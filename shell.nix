94f952a15272434287d0736425db6fb83aabd919
Dillon Beliveau <Dillon.Beliveau@coxautoinc.com>
2018-09-15 23:01:13 -0400

    Override avr-gcc to version 8.1.0, temporarily until the bug in 8.2.0 is fixed.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,3 @@
-# dfu-programmer doesn't have darwin on it's list of supported platforms
 { pkgs ? import <nixpkgs> { config = { allowUnsupportedSystem = true; }; }
 , avr ? true, arm ? true, teensy ? true }:
 
@@ -13,6 +12,13 @@ let
     "-B${avrlibc}/avr/lib/avr51"
     "-L${avrlibc}/avr/lib/avr51"
   ];
+  avrgcc = pkgs.avrgcc.overrideAttrs (_: {
+    name = "avr-gcc-8.1.0";
+    src = fetchurl {
+      url = "mirror://gcc/releases/gcc-8.1.0/gcc-8.1.0.tar.xz";
+      sha256 = "0lxil8x0jjx7zbf90cy1rli650akaa6hpk8wk8s62vk2jbwnc60x";
+    };
+  });
 in
 
 stdenv.mkDerivation {
