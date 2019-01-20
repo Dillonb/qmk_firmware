d456626ad6489224cb9caba3023c962b75d59d46
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-01-20 16:37:30 -0500

    HHKB arrows layer and TS3 mic and speakers

--- keyboards/dz60/keymaps/dgb/keymap.c
+++ keyboards/dz60/keymaps/dgb/keymap.c
@@ -6,9 +6,13 @@
 #define _FN1 1
 #define _FN2 2
 #define GAME 3
+#define HHKB 4
 
 #define _FN1SPC LT(_FN1, KC_SPC)
 #define _______ KC_TRNS
+
+#define TS3_MIC HYPR(KC_HOME)
+#define TS3_SPK HYPR(KC_END)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -16,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
           KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
           KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-          KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN1),
+          KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB),
           KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1), MO(_FN2), KC_SPC,  MO(_FN1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
      [_FN1] = LAYOUT_DGB(
@@ -30,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           _______, M(1),    M(2),    M(3),    M(4),    M(5),     M(6),    M(7),    M(8),    M(9),    M(10),    M(11),  M(12),   _______, _______,
           _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI,  RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
           _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,   BL_STEP, _______, _______, _______, _______, _______, _______,
-          _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
+          _______, TS3_MIC, TS3_SPK,  _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, _______, _______, TG(GAME), _______, _______, _______, _______, _______),
 
      [GAME] = LAYOUT_DGB(
@@ -39,6 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______, _______),
+
+     [HHKB] = LAYOUT_DGB(
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______,
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______,
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DOWN, _______, _______,
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 };
 
 int rgbmode = 0;
@@ -59,6 +70,7 @@ uint32_t layer_state_set_user(uint32_t state) {
           case BASE:
                dgb_setrgb_respecting_val(0x00, 0xFF, 0x00);
                break;
+
 
           case _FN1:
                dgb_setrgb_respecting_val(0x00, 0x80, 0x00);
