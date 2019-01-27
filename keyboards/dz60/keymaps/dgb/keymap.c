5a7e911b969526f564c8a612e4507d50c9592834
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-01-26 20:03:53 -0500

    Update DZ60

--- keyboards/dz60/keymaps/dgb/keymap.c
+++ keyboards/dz60/keymaps/dgb/keymap.c
@@ -9,19 +9,25 @@
 #define HHKB 4
 
 #define _FN1SPC LT(_FN1, KC_SPC)
+#define CTRLESC MT(MOD_LCTL, KC_ESC)
 #define _______ KC_TRNS
 
 #define TS3_MIC HYPR(KC_HOME)
 #define TS3_SPK HYPR(KC_END)
+
+#define RGB_STATIC 1
+#define RGB_FASTBREATHE 5
+#define RGB_FASTSWIRL 14
+#define RGB_FASTSNAKE 20
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      [BASE] = LAYOUT_DGB(
           KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
           KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-          KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+          CTRLESC, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
           KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB),
-          KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1), MO(_FN2), KC_SPC,  MO(_FN1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
+          KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1), MO(_FN2), _FN1SPC, MO(_FN1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
      [_FN1] = LAYOUT_DGB(
           KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______,
@@ -40,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [GAME] = LAYOUT_DGB(
           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+          KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______, _______),
 
@@ -66,27 +72,31 @@ void dgb_setrgb_respecting_val(uint8_t r, uint8_t g, uint8_t b) {
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
-     switch (biton32(state)) {
-          case BASE:
-               dgb_setrgb_respecting_val(0x00, 0xFF, 0x00);
-               break;
+    switch (biton32(state)) {
+        case BASE:
+            rgblight_mode(RGB_STATIC);
+            dgb_setrgb_respecting_val(0x00, 0xFF, 0x00);
+            break;
 
+        case _FN1:
+            rgblight_mode(RGB_FASTBREATHE);
+            dgb_setrgb_respecting_val(0x00, 0x80, 0x00);
+            break;
 
-          case _FN1:
-               dgb_setrgb_respecting_val(0x00, 0x80, 0x00);
-               break;
+        case _FN2:
+            rgblight_mode(RGB_FASTSNAKE);
+            dgb_setrgb_respecting_val(0xFF, 0xFF, 0x00);
+            break;
 
-          case _FN2:
-               dgb_setrgb_respecting_val(0xFF, 0xFF, 0x00);
-               break;
+        case GAME:
+            rgblight_mode(RGB_FASTSWIRL);
+            dgb_setrgb_respecting_val(0xFF, 0x00, 0x00);
+            break;
 
-          case GAME:
-               dgb_setrgb_respecting_val(0xFF, 0x00, 0x00);
-               break;
-
-          default:
-               dgb_setrgb_respecting_val(0xFF,  0xFF, 0xFF);
-               break;
-     }
-     return state;
+        default:
+            rgblight_mode(RGB_STATIC);
+            dgb_setrgb_respecting_val(0xFF,  0xFF, 0xFF);
+            break;
+    }
+    return state;
 }
