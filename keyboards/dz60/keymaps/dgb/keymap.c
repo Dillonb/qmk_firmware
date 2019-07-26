ee2f74003b863640fe2b2d457ee6c33a8bd4a012
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-07-25 20:52:30 -0400

    Actually back to green, remove fn1spc

--- keyboards/dz60/keymaps/dgb/keymap.c
+++ keyboards/dz60/keymaps/dgb/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
           CTRLESC, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
           KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB),
-          KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1), MO(_FN2), _FN1SPC, MO(_FN1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
+          KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1), MO(_FN2), KC_SPC,  MO(_FN1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
      [_FN1] = LAYOUT_DGB(
           KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______,
@@ -75,17 +75,17 @@ uint32_t layer_state_set_user(uint32_t state) {
     switch (biton32(state)) {
         case BASE:
             rgblight_mode(RGB_STATIC);
-            dgb_setrgb_respecting_val(0x4B, 0x00, 0x82);
+            dgb_setrgb_respecting_val(0x00, 0xFF, 0x00);
             break;
 
         case _FN1:
             rgblight_mode(RGB_FASTBREATHE);
-            dgb_setrgb_respecting_val(0x4B, 0x00, 0x82);
+            dgb_setrgb_respecting_val(0x00, 0xFF, 0x00);
             break;
 
         case _FN2:
             rgblight_mode(RGB_FASTBREATHE);
-            dgb_setrgb_respecting_val(0x4B, 0x00, 0x82);
+            dgb_setrgb_respecting_val(0x22, 0x8B, 0x22);
             break;
 
         case GAME:
